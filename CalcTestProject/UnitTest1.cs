bf9745231e0897c4f561cd90ca3a7a35c4d6f96b
CasperLC <43999061+CasperLC@users.noreply.github.com>
2019-11-12 09:25:48 +0100

    Update UnitTest1.cs

--- CalcTestProject/UnitTest1.cs
+++ CalcTestProject/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace CalcTestProject
             Calculator calc = new Calculator();
 
             var result = calc.Add(2, 2);
-            var expected = 4;
+            var expected = 5;
 
             Assert.AreEqual(expected,result);
 
